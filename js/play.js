1f6b2f941f63bc1b81b9f034332cb9d31fa80f9a
Joseph DeCommer <jdecommer@gmail.com>
2024-03-12 13:43:21 -0400

    Added Blackjack game

--- js/play.js
+++ js/play.js
@@ -6,9 +6,9 @@ const getSum2 = (a, b) => {
     return a + b
 }
 const getSum3 = (a,b) => a + b;
-console.log(getSum(4,5));
-console.log(getSum2(1,2));
-console.log(getSum3(2,3));
+// console.log(getSum(4,5));
+// console.log(getSum2(1,2));
+// console.log(getSum3(2,3));
 document.getElementById('res').textContent = `${getSum(4,5)}, ${getSum2(1,2)}, ${getSum3(2,3)}`
 const countOccurs = (str, char) => {
     charCount = 0;
@@ -19,7 +19,7 @@ const countOccurs = (str, char) => {
     }
     return charCount;
 }
-console.log(countOccurs("Shit stains are a problem sometimes", 's'));
+// console.log(countOccurs("Shit stains are a problem sometimes", 's'));
 
 let arr1 = [2, 3, 4, 500, 6, 7, 80, 900, 5, 4, 6, 3, 8]
 const maxNum = (arr) => {
@@ -33,7 +33,7 @@ const maxNum = (arr) => {
     }
     return max;
 }
-console.log(maxNum(arr1));
+// console.log(maxNum(arr1));
 
 const titleCase = (str) => {
     let words = str.toLowerCase().split(' ')
@@ -50,10 +50,10 @@ const palindrome = (str) => {
     let specChar = str.toLowerCase().replace(/[^a-z0-9]/g, '')
     let rev = specChar.split('').reverse().join('');
     if (specChar === rev) {
-        console.log(`${str} is a palindrome`)
+        // console.log(`${str} is a palindrome`)
         return `${str} is a palindrome`
     }else {
-        console.log(`${str} is not a palindrome`)
+        // console.log(`${str} is not a palindrome`)
         return `${str} is not a palindrome`
     }
 }
@@ -72,5 +72,52 @@ const removeDupes = (arr) => {
 }
 document.getElementById('no-duplicates').textContent = `Original: 2,2,3,5,4,4,7 Duplicates removed: ${removeDupes([2,2,3,5,4,4,7])}`
 
+//DICE GAME ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+document.getElementById('roll-btn').addEventListener('click', () => {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const resultText = document.getElementById('result-text')
+    const diceTotal = dice1 + dice2;
+    document.getElementById('dice-1-result').textContent = dice1;
+    document.getElementById('dice-2-result').textContent = dice2;
 
+    if(diceTotal == 7 || diceTotal == 11) {
+        resultText.textContent = `Total is ${diceTotal}. You win!`
+    }else if(diceTotal == 2 || diceTotal == 3 || diceTotal == 12) {
+        resultText.textContent = `Total is ${diceTotal}. You lose!`
+    }else {
+        resultText.textContent = `Total is ${diceTotal}. Roll again.`
+    }
+})
 
+//phone number formatter ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const phUsrIn = document.getElementById('ph-usr-in');
+const phEntBtn = document.getElementById('ph-enter-btn');
+const phResult = document.getElementById('ph-num-result');
+const formatPhoneNum = () => {
+    const numbers = phUsrIn.value;
+    const areaCode = numbers.slice(0, 3);
+    const pre = numbers.slice(3, 6);
+    const remaining = numbers.slice(6);
+
+    if(numbers.length < 10) {
+        phResult.textContent = 'Enter at least 10 digits'
+    }else {
+        setTimeout(() => {
+            phResult.textContent = '';
+        },3000);
+        phResult.textContent = `(${areaCode}) ${pre}-${remaining}`;
+    }
+};
+phEntBtn.addEventListener('click', () => {
+    formatPhoneNum();
+});
+//Deck of cards +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const suits = ['C', 'D', 'H', 'S'];
+const cardVals = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+for(let i = 0; i < suits.length; i++) {
+    for(j = 0; j < cardVals.length; j++) {
+        console.log(cardVals[j], suits[i])
+    }
+}
